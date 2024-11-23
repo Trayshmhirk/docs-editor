0c43a3d6260c9799b86c808af07b28b27dd37a6f
Frank Micheal <harlex.mikkey@gmail.com>
2024-11-23 20:58:42 +0100

    set up liveblock comment mentions

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -2,16 +2,28 @@
 import { LiveblocksProvider } from "@liveblocks/react";
 import { ClientSideSuspense } from "@liveblocks/react/suspense";
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/user.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
+   const { user: clerkUser } = useUser();
+   if (!clerkUser) redirect("/sign-in");
+
    return (
       <LiveblocksProvider
          authEndpoint="/api/liveblocks-auth"
          resolveUsers={async ({ userIds }) => {
             const users = await getClerkUsers({ userIds });
-
             return users;
+         }}
+         resolveMentionSuggestions={async ({ text, roomId }) => {
+            const roomUsers = await getDocumentUsers({
+               roomId,
+               currentUser: clerkUser.emailAddresses[0].emailAddress,
+               text,
+            });
+            return roomUsers;
          }}
       >
          <ClientSideSuspense fallback={<Loader />}>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { clerkClient } from "@clerk/nextjs/server";
+import { liveblocks } from "../liveblocks";
 
 export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
    const client = clerkClient();
@@ -25,5 +26,36 @@ export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
       return JSON.parse(JSON.stringify(sortedUsers));
    } catch (error) {
       console.log(`Error fetching users: ${error}`);
+   }
+};
+
+export const getDocumentUsers = async ({
+   roomId,
+   currentUser,
+   text,
+}: {
+   roomId: string;
+   currentUser: string;
+   text: string;
+}) => {
+   try {
+      const room = await liveblocks.getRoom(roomId);
+      const users = Object.keys(room.usersAccesses).filter(
+         (email) => email !== currentUser
+      );
+
+      if (text.length) {
+         const lowerCaseText = text.toLowerCase();
+
+         const filteredUsers = users.filter((email) =>
+            email.toLocaleLowerCase().includes(lowerCaseText)
+         );
+
+         return JSON.parse(JSON.stringify(filteredUsers));
+      }
+
+      return JSON.parse(JSON.stringify(users));
+   } catch (error) {
+      console.log(`Error fetching document users: ${error}`);
    }
 };
