f194377d277f95624a281c876e235d7e68225073
Frank Micheal <harlex.mikkey@gmail.com>
2024-11-23 17:33:13 +0100

    awaiting route params

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -4,7 +4,9 @@ import { getClerkUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-const Document = async ({ params: { id } }: SearchParamProps) => {
+const Document = async ({ params }: SearchParamProps) => {
+   const { id } = await Promise.resolve(params);
+
    const clerkUser = await currentUser();
    if (!clerkUser) {
       redirect("/sign-in");
