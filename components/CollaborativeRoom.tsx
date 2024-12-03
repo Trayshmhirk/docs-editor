dcc465e498fe0c1a3143711eaec3c2b6f3abc00b
Frank Micheal <harlex.mikkey@gmail.com>
2024-12-03 21:59:53 +0100

    adjust tabs width with prettier

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -14,131 +14,123 @@ import { updateDocument } from "@/lib/actions/room.actions";
 import ShareModal from "./ShareModal";
 
 const CollaborativeRoom = ({
-   roomId,
-   roomMetadata,
-   users,
-   currentUserType,
+  roomId,
+  roomMetadata,
+  users,
+  currentUserType,
 }: CollaborativeRoomProps) => {
-   const [editing, setEditing] = useState(false);
-   const [loading, setLoading] = useState(false);
-   const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
+  const [editing, setEditing] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
 
-   const containerRef = useRef<HTMLDivElement>(null);
-   const inputRef = useRef<HTMLInputElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-   const updateTitleHandler = async (
-      e: React.KeyboardEvent<HTMLInputElement>
-   ) => {
-      if (e.key === "Enter") {
-         setLoading(true);
+  const updateTitleHandler = async (
+    e: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (e.key === "Enter") {
+      setLoading(true);
 
-         try {
-            if (documentTitle !== roomMetadata.title) {
-               // update documents
-               const updatedDocument = await updateDocument(
-                  roomId,
-                  documentTitle
-               );
+      try {
+        if (documentTitle !== roomMetadata.title) {
+          // update documents
+          const updatedDocument = await updateDocument(roomId, documentTitle);
 
-               if (updatedDocument) {
-                  setEditing(false);
-               }
-            }
-         } catch (error) {
-            console.error(`Error updating document title: ${error}`);
-         }
-
-         setLoading(false);
-      }
-   };
-
-   useEffect(() => {
-      const handleClickOutside = (e: MouseEvent) => {
-         if (
-            containerRef.current &&
-            !containerRef.current.contains(e.target as Node)
-         ) {
+          if (updatedDocument) {
             setEditing(false);
-            updateDocument(roomId, documentTitle);
-         }
-      };
+          }
+        }
+      } catch (error) {
+        console.error(`Error updating document title: ${error}`);
+      }
 
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(e.target as Node)
+      ) {
+        setEditing(false);
+        updateDocument(roomId, documentTitle);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
       document.addEventListener("mousedown", handleClickOutside);
+    };
+  }, [roomId, documentTitle]);
 
-      return () => {
-         document.addEventListener("mousedown", handleClickOutside);
-      };
-   }, [roomId, documentTitle]);
+  // useEffect(() => {
+  //    if (editing && inputRef.current) {
+  //       inputRef.current.focus();
+  //    }
+  // }, [editing]);
 
-   // useEffect(() => {
-   //    if (editing && inputRef.current) {
-   //       inputRef.current.focus();
-   //    }
-   // }, [editing]);
+  return (
+    <RoomProvider id={roomId}>
+      <ClientSideSuspense fallback={<Loader />}>
+        <div className="flex size-full flex-1 flex-col items-center overflow-hidden">
+          <Header>
+            <div ref={containerRef} className="flex w-fit items-center gap-2">
+              {editing && !loading ? (
+                <Input
+                  type="text"
+                  value={documentTitle}
+                  ref={inputRef}
+                  placeholder="Enter title"
+                  onChange={(e) => setDocumentTitle(e.target.value)}
+                  onKeyDown={updateTitleHandler}
+                  disabled={!editing}
+                  className="document-title-input"
+                />
+              ) : (
+                <p className="line-clamp-1 border-dark-400 text-base font-semibold leading-[24px] sm:pl-0 sm:text-xl">
+                  {documentTitle}
+                </p>
+              )}
 
-   return (
-      <RoomProvider id={roomId}>
-         <ClientSideSuspense fallback={<Loader />}>
-            <div className="flex size-full flex-1 flex-col items-center overflow-hidden">
-               <Header>
-                  <div
-                     ref={containerRef}
-                     className="flex w-fit items-center gap-2"
-                  >
-                     {editing && !loading ? (
-                        <Input
-                           type="text"
-                           value={documentTitle}
-                           ref={inputRef}
-                           placeholder="Enter title"
-                           onChange={(e) => setDocumentTitle(e.target.value)}
-                           onKeyDown={updateTitleHandler}
-                           disabled={!editing}
-                           className="document-title-input"
-                        />
-                     ) : (
-                        <p className="line-clamp-1 border-dark-400 text-base font-semibold leading-[24px] sm:pl-0 sm:text-xl">
-                           {documentTitle}
-                        </p>
-                     )}
-
-                     {currentUserType === "editor" && !editing && (
-                        <SquarePen
-                           className="size-6 cursor-pointer"
-                           onClick={() => setEditing(true)}
-                        />
-                     )}
-                     {currentUserType !== "editor" && !editing && (
-                        <p className="rounded-md bg-dark-400/50 px-2 py-0.5 text-xs text-blue-100/50">
-                           View only
-                        </p>
-                     )}
-                     {loading && (
-                        <p className="text-sm text-gray-100">saving...</p>
-                     )}
-                  </div>
-
-                  <div className="flex items-center gap-4 justify-center">
-                     <ActiveCollaborators />
-                     <ShareModal
-                        roomId={roomId}
-                        collaborators={users}
-                        creatorId={roomMetadata.creatorId}
-                        currentUserType={currentUserType}
-                     />
-                     <SignedOut>
-                        <SignInButton />
-                     </SignedOut>
-                     <SignedIn>
-                        <UserButton />
-                     </SignedIn>
-                  </div>
-               </Header>
-               <Editor roomId={roomId} currentUserType={currentUserType} />
+              {currentUserType === "editor" && !editing && (
+                <SquarePen
+                  className="size-6 cursor-pointer"
+                  onClick={() => setEditing(true)}
+                />
+              )}
+              {currentUserType !== "editor" && !editing && (
+                <p className="rounded-md bg-dark-400/50 px-2 py-0.5 text-xs text-blue-100/50">
+                  View only
+                </p>
+              )}
+              {loading && <p className="text-sm text-gray-100">saving...</p>}
             </div>
-         </ClientSideSuspense>
-      </RoomProvider>
-   );
+
+            <div className="flex items-center gap-4 justify-center">
+              <ActiveCollaborators />
+              <ShareModal
+                roomId={roomId}
+                collaborators={users}
+                creatorId={roomMetadata.creatorId}
+                currentUserType={currentUserType}
+              />
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </Header>
+          <Editor roomId={roomId} currentUserType={currentUserType} />
+        </div>
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
 };
 
 export default CollaborativeRoom;
--- components/UserTypeSelector.tsx
+++ components/UserTypeSelector.tsx
@@ -1,49 +1,49 @@
 import React from "react";
 import {
-   Select,
-   SelectContent,
-   SelectItem,
-   SelectTrigger,
-   SelectValue,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/ui/select";
 
 const UserTypeSelector = ({
-   userType,
-   setUserType,
-   onClickHandler,
+  userType,
+  setUserType,
+  onClickHandler,
 }: UserTypeSelectorParams) => {
-   const accessChangeHandler = (type: UserType) => {
-      setUserType(type);
+  const accessChangeHandler = (type: UserType) => {
+    setUserType(type);
 
-      if (onClickHandler) {
-         onClickHandler(type);
-      }
-   };
+    if (onClickHandler) {
+      onClickHandler(type);
+    }
+  };
 
-   return (
-      <Select
-         value={userType}
-         onValueChange={(type: UserType) => accessChangeHandler(type)}
-      >
-         <SelectTrigger className="w-fit border-none bg-transparent text-[#d3d3d3] focus:ring-0 focus:ring-offset-0">
-            <SelectValue />
-         </SelectTrigger>
-         <SelectContent className="border-none bg-[#111111]">
-            <SelectItem
-               value="viewer"
-               className="cursor-pointer bg-[#111111] text-[#b8b8b8] focus:bg-[#2d2d2d] hover:bg-[#2d2d2d] focus:text-[#efefef]"
-            >
-               Can view
-            </SelectItem>
-            <SelectItem
-               value="editor"
-               className="cursor-pointer bg-[#111111] text-[#b8b8b8] focus:bg-[#2d2d2d] hover:bg-[#2d2d2d] focus:text-[#efefef]"
-            >
-               Can edit
-            </SelectItem>
-         </SelectContent>
-      </Select>
-   );
+  return (
+    <Select
+      value={userType}
+      onValueChange={(type: UserType) => accessChangeHandler(type)}
+    >
+      <SelectTrigger className="w-fit border-none bg-transparent text-[#d3d3d3] focus:ring-0 focus:ring-offset-0">
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent className="border-none bg-[#111111]">
+        <SelectItem
+          value="viewer"
+          className="cursor-pointer bg-[#111111] text-[#b8b8b8] focus:bg-[#2d2d2d] hover:bg-[#2d2d2d] focus:text-[#efefef]"
+        >
+          Can view
+        </SelectItem>
+        <SelectItem
+          value="editor"
+          className="cursor-pointer bg-[#111111] text-[#b8b8b8] focus:bg-[#2d2d2d] hover:bg-[#2d2d2d] focus:text-[#efefef]"
+        >
+          Can edit
+        </SelectItem>
+      </SelectContent>
+    </Select>
+  );
 };
 
 export default UserTypeSelector;
