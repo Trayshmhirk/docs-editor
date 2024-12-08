a50979c0a73b8d7a4999aa54a7259df6dbd2a941
Frank Micheal <harlex.mikkey@gmail.com>
2024-12-08 21:05:27 +0100

    refactor box shadow for clerk signin and signup components

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,6 @@
 
 import { dark } from "@clerk/themes";
 import { useTheme } from "next-themes";
-
 import { SignIn } from "@clerk/nextjs";
 
 const SignInPage = () => {
@@ -14,9 +13,12 @@ const SignInPage = () => {
         appearance={{
           baseTheme: resolvedTheme === "dark" ? dark : undefined,
           elements: {
-            cardBox: "",
-            card: "dark:bg-[#1f1f1f]",
+            cardBox:
+              "border border-[#eeeeee] dark:border-[#181818] shadow-xl dark:shadow-xl-dark",
+            card: "dark:bg-[#1f1f1f] shadow-sm dark:shadow-md-dark",
             footer: "dark:bg-[#0f0f0f]",
+            socialButtonsBlockButton:
+              "hover:shadow-md dark:hover:shadow-sm-dark duration-300",
           },
         }}
       />
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,27 @@
+"use client";
+
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 import { SignUp } from "@clerk/nextjs";
 
 const SignUpPage = () => {
+  const { resolvedTheme } = useTheme();
+
   return (
     <main className="flex h-screen w-full flex-col items-center justify-center gap-10">
-      <SignUp />
+      <SignUp
+        appearance={{
+          baseTheme: resolvedTheme === "dark" ? dark : undefined,
+          elements: {
+            cardBox:
+              "border border-[#eeeeee] dark:border-[#181818] shadow-xl dark:shadow-xl-dark",
+            card: "dark:bg-[#1f1f1f] shadow-sm dark:shadow-md-dark",
+            footer: "dark:bg-[#0f0f0f]",
+            socialButtonsBlockButton:
+              "hover:shadow-md dark:hover:shadow-sm-dark duration-300",
+          },
+        }}
+      />
     </main>
   );
 };
