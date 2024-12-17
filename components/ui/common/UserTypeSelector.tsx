219f01f31f773f6c178456eaaeb586ba7a9a3aa4
Frank Micheal <harlex.mikkey@gmail.com>
2024-12-17 22:37:28 +0100

    fix light theme for usertypeselector

--- components/ui/common/UserTypeSelector.tsx
+++ components/ui/common/UserTypeSelector.tsx
@@ -25,19 +25,19 @@ const UserTypeSelector = ({
       value={userType}
       onValueChange={(type: UserType) => accessChangeHandler(type)}
     >
-      <SelectTrigger className="w-fit border-none bg-transparent text-[#d3d3d3] focus:ring-0 focus:ring-offset-0">
+      <SelectTrigger className="w-fit border-none bg-transparent dark:bg-transparent text-[#828282] dark:text-[#d3d3d3] focus:ring-0 focus:ring-offset-0">
         <SelectValue />
       </SelectTrigger>
-      <SelectContent className="border-none bg-[#111111]">
+      <SelectContent className="border border-[#d8d8d8] dark:border-[#7a7a7a] dark:bg-[#111111]">
         <SelectItem
           value="viewer"
-          className="cursor-pointer bg-[#111111] text-[#b8b8b8] focus:bg-[#2d2d2d] hover:bg-[#2d2d2d] focus:text-[#efefef]"
+          className="cursor-pointer dark:bg-[#111111] dark:text-[#b8b8b8] focus:bg-[#e9e9e9] dark:focus:bg-[#2d2d2d] dark:hover:bg-[#2d2d2d] dark:focus:text-[#efefef]"
         >
           Can view
         </SelectItem>
         <SelectItem
           value="editor"
-          className="cursor-pointer bg-[#111111] text-[#b8b8b8] focus:bg-[#2d2d2d] hover:bg-[#2d2d2d] focus:text-[#efefef]"
+          className="cursor-pointer dark:bg-[#111111] text-[#b8b8b8] focus:bg-[#e9e9e9] dark:focus:bg-[#2d2d2d] dark:hover:bg-[#2d2d2d] dark:focus:text-[#efefef]"
         >
           Can edit
         </SelectItem>
