e2294da78c55199a780eef9552c7073e1abd9091
Frank Micheal <harlex.mikkey@gmail.com>
2024-12-17 22:37:57 +0100

    minor change

--- components/ui/common/ToggleTheme.tsx
+++ components/ui/common/ToggleTheme.tsx
@@ -23,7 +23,7 @@ export function ToggleTheme({ isEditor }: { isEditor?: boolean }) {
           className={`text-[#1e1e1e] dark:text-white hover:bg-[#fcfcfc] dark:hover:bg-[#383838] transition-all   ${
             isEditor
               ? "bg-transparent dark:bg-transparent hover-shadow"
-              : "bg-white dark:bg-[#2a2a2a] border border-[#d1d1d1]  dark:border-[#7a7a7a]"
+              : "bg-white dark:bg-[#2a2a2a] border border-[#d1d1d1] dark:border-[#7a7a7a]"
           }`}
         >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
