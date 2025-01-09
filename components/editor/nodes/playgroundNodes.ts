a4510ef7e9bb9b968ebf9efbfedabdd4a627c216
Frank Micheal <harlex.mikkey@gmail.com>
2025-01-09 22:32:17 +0100

    discard commented code

--- components/editor/nodes/playgroundNodes.ts
+++ components/editor/nodes/playgroundNodes.ts
@@ -10,27 +10,6 @@ import { HorizontalRuleNode } from "@lexical/react/LexicalHorizontalRuleNode";
 import { HeadingNode, QuoteNode } from "@lexical/rich-text";
 import { TableCellNode, TableNode, TableRowNode } from "@lexical/table";
 
-// import { CollapsibleContainerNode } from "../plugins/CollapsiblePlugin/CollapsibleContainerNode";
-// import { CollapsibleContentNode } from "../plugins/CollapsiblePlugin/CollapsibleContentNode";
-// import { CollapsibleTitleNode } from "../plugins/CollapsiblePlugin/CollapsibleTitleNode";
-// import { AutocompleteNode } from "./AutocompleteNode";
-// import { EmojiNode } from "./EmojiNode";
-// import { EquationNode } from "./EquationNode";
-// import { ExcalidrawNode } from "./ExcalidrawNode";
-// import { FigmaNode } from "./FigmaNode";
-// import { ImageNode } from "./ImageNode";
-// import { InlineImageNode } from "./InlineImageNode/InlineImageNode";
-// import { KeywordNode } from "./KeywordNode";
-// import { LayoutContainerNode } from "./LayoutContainerNode";
-// import { LayoutItemNode } from "./LayoutItemNode";
-// import { MentionNode } from "./MentionNode";
-// import { PageBreakNode } from "./PageBreakNode";
-// import { PollNode } from "./PollNode";
-// import { SpecialTextNode } from "./SpecialTextNode";
-// import { StickyNode } from "./StickyNode";
-// import { TweetNode } from "./TweetNode";
-// import { YouTubeNode } from "./YouTubeNode";
-
 const PlaygroundNodes: Array<Klass<LexicalNode>> = [
   HeadingNode,
   ListNode,
@@ -47,26 +26,6 @@ const PlaygroundNodes: Array<Klass<LexicalNode>> = [
   OverflowNode,
   HorizontalRuleNode,
   MarkNode,
-  // PollNode,
-  // StickyNode,
-  // ImageNode,
-  // InlineImageNode,
-  // MentionNode,
-  // EmojiNode,
-  // ExcalidrawNode,
-  // EquationNode,
-  // AutocompleteNode,
-  // KeywordNode,
-  // TweetNode,
-  // YouTubeNode,
-  // FigmaNode,
-  // CollapsibleContainerNode,
-  // CollapsibleContentNode,
-  // CollapsibleTitleNode,
-  // PageBreakNode,
-  // LayoutContainerNode,
-  // LayoutItemNode,
-  // SpecialTextNode,
 ];
 
 export default PlaygroundNodes;
